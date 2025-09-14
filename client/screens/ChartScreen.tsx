09bc9fab55fbc053f1910553b99adf9563cef7e8
vinal <vinchau01@gmail.com>
2025-09-13 20:38:35 -0700

    slowly shaping it out

--- client/screens/ChartScreen.tsx
+++ client/screens/ChartScreen.tsx
@@ -3,3 +3,7 @@ import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import Svg, { Circle, Line, G, Text as SvgText } from 'react-native-svg';
 import { birthToUTC } from '../lib/time';
 import { computeNatalPlanets, PlanetPos, findAspects, Aspect } from '../lib/astro';
+
+const ZODIAC = [ 'Ar, Ta', 'Ge', 'Ca', 'Le', 'Vi', 'Li', 'Sc', 'Sg', 'Cp', 'Aq', 'Pi' ]
+const signOf = (lon: number) => Math.floor(lon / 30)
+const degInSign = (lon: number) => (lon % 30 + 30) % 30;
