f44556591b67c9acc78a291c983676633398fb83
vinal <vinchau01@gmail.com>
2025-09-13 20:27:50 -0700

    setting up imports for chart screen

---
+++ client/screens/ChartScreen.tsx
@@ -0,0 +1,3 @@
+import React, { useMemo } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import Svg, { circle, Line, G, Text as SvgText } from 'react-native-svg';
