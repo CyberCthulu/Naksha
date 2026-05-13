213578cc333a1e0f501c2899f097d8c506200da8
vinal <vinchau01@gmail.com>
2026-05-12 21:30:30 -0700

    Add Expo ESLint setup

---
+++ client/eslint.config.js
@@ -0,0 +1,38 @@
+const expoConfig = require('eslint-config-expo/flat')
+const { defineConfig } = require('eslint/config')
+const globals = require('globals')
+
+module.exports = defineConfig([
+  {
+    ignores: [
+      'android/',
+      'node_modules/',
+      'dist/',
+      'build/',
+      'coverage/',
+      '.expo/',
+    ],
+  },
+  ...expoConfig,
+  {
+    files: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      'react/display-name': 'off',
+    },
+  },
+  {
+    files: ['*.config.js', 'metro.config.js', 'jest.config.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      sourceType: 'commonjs',
+    },
+  },
+])
